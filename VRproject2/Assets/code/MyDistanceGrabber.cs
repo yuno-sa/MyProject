e138f411dbacccedd2e35e2f71887b4bd2f97fee
yuno-sa <g19935sa@gm.tsuda.ac.jp>
2022-09-21 14:01:21 +0900

    commitfirst

---
+++ VRproject2/Assets/code/MyDistanceGrabber.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class MyDistanceGrabber : MonoBehaviour
+{
+    public enum HandType
+    {
+        LEFT,
+        RIGHT
+    }
+
+    [SerializeField] private HandType _handType = HandType.LEFT;
+    [SerializeField] private float _castCircleRadius = 0.2f;
+    [SerializeField] private float _maxCastDistance = 3f;
+    [SerializeField, Range(0f, 1f)] private float _powerAdjust = 0.2f;
+    [SerializeField] private float _minReleasePower = 1f;
+    [SerializeField] private float _maxReleasePower = 100f;
+
+    private MyGrabbable _targetGrabbable;
+    private MyGrabbable _grabbedGrabbable;
+
+    void Start()
+    {
+        this.FixedUpdateAsObservable().Subscribe(_ =>
+        {
+            // 掴めるオブジェクトの判定
+            DetectGrabbable();
+        }).AddTo(this);
+
+        // 右か左かによってボタン、コントローラーの切り分け
+        OVRInput.RawButton buttonType = OVRInput.RawButton.LHandTrigger;
+        OVRInput.Controller controller = OVRInput.Controller.LTouch;
+        switch (_handType)
+        {
+            case HandType.LEFT:
+                buttonType = OVRInput.RawButton.LHandTrigger;
+                controller = OVRInput.Controller.LTouch;
+                break;
+            case HandType.RIGHT:
+                buttonType = OVRInput.RawButton.RHandTrigger;
+                controller = OVRInput.Controller.RTouch;
+                break;
+        }
+
+        // 中指ボタンが押されたとき
+        this.UpdateAsObservable()
+            .Where(_ => OVRInput.GetDown(buttonType))
+            .Subscribe(_ =>
+            {
+                if (_targetGrabbable != null)
+                {
+                    _targetGrabbable.Grab(transform);
+                    _grabbedGrabbable = _targetGrabbable;
+                }
+            }).AddTo(this);
+
+        // 中指ボタンが離されたとき
+        this.UpdateAsObservable()
+            .Where(_ => OVRInput.GetUp(buttonType))
+            .Subscribe(_ =>
+            {
+                if (_grabbedGrabbable != null)
+                {
+                    Vector3 dir = transform.forward;
+                    float mag = OVRInput.GetLocalControllerAcceleration(controller).magnitude;
+                    if (mag < _minReleasePower)
+                    {
+                        mag = 0f;
+                    }
+
+                    if (mag > _maxReleasePower)
+                    {
+                        mag = _maxReleasePower;
+                    }
+                    Vector3 acc = dir.normalized * (mag * _powerAdjust);
+                    _grabbedGrabbable.Release(acc);
+                    _grabbedGrabbable = null;
+                }
+            }).AddTo(this);
+    }
+
+    /// <summary>
+    /// 掴めるオブジェクトを検知する
+    /// </summary>
+    void DetectGrabbable()
+    {
+        RaycastHit hit;
+        Vector3 p1 = transform.position;
+        int layerMask = LayerMask.GetMask(new string[] {"Grabbable"});
+        if (Physics.SphereCast(p1, _castCircleRadius, transform.forward, out hit, _maxCastDistance, layerMask))
+        {
+            var grabbable = hit.collider.GetComponent<MyGrabbable>();
+            if (grabbable)
+            {
+                if (_targetGrabbable != null)
+                {
+                    _targetGrabbable.OutLineEnabled = false;
+                }
+                grabbable.OutLineEnabled = true;
+                _targetGrabbable = grabbable;
+            }
+        }
+        else
+        {
+            if (_targetGrabbable != null)
+            {
+                _targetGrabbable.OutLineEnabled = false;
+                _targetGrabbable = null;
+            }
+        }
+    }
+}
---
+++ VRproject2/Assets/code/MyGrabbable.cs
@@ -0,0 +1,84 @@
+using System;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+[RequireComponent(typeof(Outline))]
+public class MyGrabbable : MonoBehaviour
+{
+    public bool IsGrabbed
+    {
+        get => _isGrabbed;
+    }
+
+    private Rigidbody _rigidbody;
+    private Transform _defaultParent;
+    private Outline _outline;
+    private bool _isGrabbed = false;
+
+    public bool OutLineEnabled
+    {
+        get { return _outline.enabled; }
+        set { _outline.enabled = value; }
+    }
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _outline = GetComponent<Outline>();
+        _outline.enabled = false;
+    }
+
+    void Start()
+    {
+        _defaultParent = transform.parent;
+    }
+
+    private IDisposable _grabDisposable;
+    public void Grab(Transform handTrans)
+    {
+        if (_isGrabbed)
+        {
+            return;
+        }
+
+        if (_rigidbody != null)
+        {
+            _rigidbody.isKinematic = true;
+        }
+        _isGrabbed = true;
+        _grabDisposable?.Dispose();
+        _grabDisposable = this.UpdateAsObservable()
+            // 手まで限りなく近くなるまで自分を近づける
+            .TakeWhile(_ => (transform.position - handTrans.position).sqrMagnitude > 0.1f * 0.1f).Subscribe(
+                _ =>
+                {
+                    // 手までオブジェクトを近づける
+                    Vector3 dir = (handTrans.position - transform.position).normalized;
+                    transform.position += dir * (10f * Time.deltaTime);
+                }, () =>
+                {
+                    // 購読完了時
+                    // 手まで限りなく近くなった場合、親を手にして完了
+                    transform.parent = handTrans;
+                    transform.position = handTrans.position;
+                }).AddTo(this);
+    }
+
+    public void Release(Vector3 controllerAcc)
+    {
+        if (!_isGrabbed)
+        {
+            return;
+        }
+
+        if (_rigidbody != null)
+        {
+            _rigidbody.isKinematic = false;
+            _rigidbody.AddForce(controllerAcc, ForceMode.Impulse);
+        }
+        _grabDisposable?.Dispose();
+        _isGrabbed = false;
+        transform.parent = _defaultParent;
+    }
+}
