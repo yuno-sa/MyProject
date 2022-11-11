089985effd116d3dfd786673a6f9a24476c9b9c7
yuno-sa <75403468+yuno-sa@users.noreply.github.com>
2022-11-11 15:34:44 +0900

    Update ButtonScript.cs

--- VRproject2/Assets/code/ButtonScript.cs
+++ VRproject2/Assets/code/ButtonScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-//using UnityEngine.SceneManagement;
 using UniRx;
 using UniRx.Triggers;
 using System.Linq;
@@ -12,19 +11,16 @@ public class ButtonScript : MonoBehaviour
 {
 
     [SerializeField, Tooltip("ボタン")]
-    //public GameObject button;
     private TextMeshProUGUI buttontext;
     private Vector3 initialPos;
     public Transform target;
-    //private GameObject[] items;
 
     // Start is called before the first frame update
     void Start()
     {
         buttontext = transform.GetChild(0).gameObject.GetComponentInChildren<TextMeshProUGUI>();
         initialPos = new Vector3(transform.position.x,target.position.y-transform.position.y,transform.position.z);
-        transform.position = new Vector3(100,100,100);
-        //items = GameObject.FindGameObjectsWithTag("All");
+        transform.position = new Vector3(100,0,100);
     }
 
     // Update is called once per frame
@@ -34,7 +30,7 @@ public class ButtonScript : MonoBehaviour
     }
     public void Clear()
     {
-        transform.position = new Vector3(100,100,100);
+        transform.position = new Vector3(100,0,100);
     }
     public void ChangeText(string s)
     {
@@ -44,25 +40,21 @@ public class ButtonScript : MonoBehaviour
     }
     public void OnClick()
     {
-        //ここにシーン遷移を入れる？
-        //ボタンの文字を参照。
-        //ifで分岐、それぞれのタグだけ集めたシーンを作って移動するようにする
-        //だといちいち書き換えないといけないから、シーンの名前をタグの名前と共通にして、シーンを名前から検索する
-        /*
-        SceneManager.LoadScene("MainScene");シーン遷移
-        SceneManager.LoadScene(ボタンのテキストを取得して、そのまま入れる→遷移できるようにする);
-        */
-        /*GameObject[] goArray = GameObject.FindObjectsOfType<GameObject>();
-        for(int i = 0;i<goArray.Count;i++)
+        buttontext = transform.GetChild(0).gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        int i = 0;
+        foreach (var potentialTarget in FindObjectsOfType<CustomTag>())
         {
-            if(!goArray[i].HasTag(buttontext.ToString()))
+            potentialTarget.gameObject.GetComponent<MyGrabbable>().Release();
+            if (potentialTarget.HasTag(buttontext.text))
             {
-                goArray[i].SetActive(false);
+                potentialTarget.gameObject.transform.position = new Vector3((float)(-1+i*0.3),(float)1.5,(float)2.2);
+                i++;
             }
-        }*/
-        GameObject cube = GameObject.Find("Cube");
-        cube.SetActive(false);
-        //GameObject[] targets = GameObject.FindGameObjectsWithTag(buttontext.ToString());
-        //buttontext.text = items.Count().ToString();
+            else
+            {
+                potentialTarget.gameObject.transform.position = new Vector3(100,0,100);
+            }
+        }
+        i = 0;
     }
 }
